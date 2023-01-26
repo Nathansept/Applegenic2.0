785b22a9c42d4c8c828733ab2cc827c0614bf416
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2023-01-26 16:31:16 +0700

    merge

--- index.php
+++ index.php
@@ -246,6 +246,7 @@ function kirimContactForm(){
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         <strong><div class="cinta">Copyright © 2023</i> by <a href="https://github.com/Nathansept/Applegenic2.0" target="_blank">Applegenic</a></div></strong>
 =======
         <strong><div class="cinta">Copyright © 2017 - Made with <i class="fa fa-heart pulse"></i> by <a href="http://januriawan.github.io" target="_blank">Januriawan</a></div></strong>
@@ -256,6 +257,9 @@ function kirimContactForm(){
 =======
         <strong><div class="cinta">Copyright © 2017 - Made with <i class="fa fa-heart pulse"></i> by <a href="http://januriawan.github.io" target="_blank">Januriawan</a></div></strong>
 >>>>>>> parent of 12461f2 (second)
+=======
+        <strong><div class="cinta">Copyright © 2023</i> by <a href="https://github.com/Nathansept/Applegenic2.0" target="_blank">Applegenic</a></div></strong>
+>>>>>>> parent of c770cd4 (merge)
       </footer>
       <!-- Add the sidebar's background. This div must be placed
            immediately after the control sidebar -->
